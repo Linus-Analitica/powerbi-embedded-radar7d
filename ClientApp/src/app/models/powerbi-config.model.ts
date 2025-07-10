fcebb97b24069bff646a2ee5df29c191c481ad98
Francisco Javier Ibáñez Canales <t-franibanez@itesm.mx>
2025-07-10 12:17:08 -0600

    Version equipo tec

--- ClientApp/src/app/models/powerbi-config.model.ts
+++ ClientApp/src/app/models/powerbi-config.model.ts
@@ -1,3 +1,4 @@
+
 export interface PowerBiConfig {
     TenantID: string;
     ClientID: string;
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Azure;
+﻿﻿using Azure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
